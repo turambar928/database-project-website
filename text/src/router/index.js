1525823ee9f7419d6b702dd1b40aecbd4f9e449f
SJZLZZHR <3590598688@qq.com>
2024-07-18 23:44:50 +0800

    commit text

--- text/src/router/index.js
+++ text/src/router/index.js
@@ -1,11 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import DashboardComponent from '../components/Dashboard.vue'
+import AppBarComponent from '../components/AppBar.vue'
+import MyContentComponent from '../components/MyContent.vue'
+import MySidebarComponent from '../components/MySidebar.vue'
 
 const routes = [
   {
-    path: '/',
-    name: 'Dashboard',
-    component: DashboardComponent
+    path: '/content',
+    name: 'AppBar',
+    component: AppBarComponent
+  },
+  {
+    path: '/content',
+    name: 'MyContent',
+    component: MyContentComponent
+  },
+  {
+    path: '/content',
+    name: 'MySidebar',
+    component: MySidebarComponent
   }
 ]
 
@@ -15,3 +27,4 @@ const router = createRouter({
 })
 
 export default router
+
